6d150706ccfd92e19298e0a00cca3fe93163bf68
João Ceia <joao.p.ceia@gmail.com>
2022-01-04 22:10:19 +0000

    added destructor and comments

--- ex02/headers/MutantStack.hpp
+++ ex02/headers/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 00:14:39 by jpceia            #+#    #+#             */
-/*   Updated: 2022/01/04 22:08:28 by jpceia           ###   ########.fr       */
+/*   Updated: 2022/01/04 22:10:08 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,26 @@
 template <typename T>
 class MutantStack : public std::stack<T>
 {
-    public:
-        MutantStack(void) {};
-        MutantStack(const MutantStack& rhs) { *this = rhs; };
+public:
+    // Constructors
+    MutantStack(void) {};
+    MutantStack(const MutantStack& rhs) { *this = rhs; };
 
-        MutantStack& operator=(const MutantStack& rhs)
-        {
-            std::stack<T>::operator=(rhs);
-            return *this;
-        };
-        
-        typedef typename std::stack<T>::container_type::iterator iterator;
-        
-        iterator begin(void) { return std::stack<T>::c.begin(); };
-        iterator end(void) { return std::stack<T>::c.end(); };
+    // Destructor
+    ~MutantStack(void) {};
+
+    // Assignation operator overload
+    MutantStack& operator=(const MutantStack& rhs)
+    {
+        std::stack<T>::operator=(rhs);
+        return *this;
+    };
+    
+    // Iterator
+    typedef typename std::stack<T>::container_type::iterator iterator;
+    
+    iterator begin(void) { return std::stack<T>::c.begin(); };
+    iterator end(void) { return std::stack<T>::c.end(); };
 };
 
 #endif
